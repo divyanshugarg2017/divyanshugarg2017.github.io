8dab9a108f34864238c13ab84a5ea64bd0c8679e
Divyanshu Garg <divyanshugarg1995@gmail.com>
2021-01-18 04:41:59 -0600

    added covid tracker project

---
+++ covid19/precache-manifest.f4c8191c7489a1fa166faadce7fd8eb4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41afffb77936bcf1042e26e5e9c9422e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "839393833adbf0a5c729",
+    "url": "/static/css/2.a4567992.chunk.css"
+  },
+  {
+    "revision": "b8e9cbd2ea7d8d779191",
+    "url": "/static/css/main.902dd38f.chunk.css"
+  },
+  {
+    "revision": "839393833adbf0a5c729",
+    "url": "/static/js/2.74591f71.chunk.js"
+  },
+  {
+    "revision": "0b110a718d0a8fc6b16693cbaab585fa",
+    "url": "/static/js/2.74591f71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8e9cbd2ea7d8d779191",
+    "url": "/static/js/main.f6a7c84f.chunk.js"
+  },
+  {
+    "revision": "4df097abbea25f86dac0",
+    "url": "/static/js/runtime-main.f10a8730.js"
+  }
+]);
